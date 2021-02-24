bd60e2f14c5237c0f673cd9ac0e86704444fb85e
Enrai22 <63306823+Enrai22@users.noreply.github.com>
2021-02-24 20:41:52 +0900

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,7 +1,1 @@
-require 'discordrb'
-bot = Discordrb::Bot.new token: 'ODEzNjQ3MjkyOTQxNzI5ODEz.YDSWFw.lHsvQr9KUvyrLbJg--wkxj3RQyY'
-bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
-end
 
-bot.run
